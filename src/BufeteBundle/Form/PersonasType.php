64541c06fc31b022bbdf1af0e4049254d801e656
Adder <a.j.orozco038@hotmail.com>
2017-09-23 19:27:32 -0600

    agregar personas

--- src/BufeteBundle/Form/PersonasType.php
+++ src/BufeteBundle/Form/PersonasType.php
@@ -5,6 +5,11 @@ namespace BufeteBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class PersonasType extends AbstractType
 {
@@ -13,16 +18,67 @@ class PersonasType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nombrePersona')->add('telefonoPersona')->add('tel2Persona')->add('dpiPersona')->add('direccionPersona')->add('dir2Persona')->add('emailPersona')->add('usuarioPersona')->add('passPersona')->add('foto')->add('estadoPersona')->add('role')->add('idBufete');
+
+      $this->carneEnvio = $options['carneEnvio'];
+      $this->nomEnvio = $options['nombreEnvio'];
+      $this->telEnvio = $options['telefonoEnvio'];
+      $this->dirEnvio = $options['direccionEnvio'];
+      $this->corEnvio = $options['correoEnvio'];
+      $this->passEnvio = $options['passEnvio'];
+
+      $builder
+
+      //form DATOS PERSONALES
+
+        ->add('nombrePersona',TextType::Class, array ("data"=>$this->nomEnvio))
+        ->add('telefonoPersona',TextType::Class, array ("data"=>$this->telEnvio))
+        ->add('tel2Persona')
+        ->add('dpiPersona')
+        ->add('direccionPersona',TextType::Class, array ("data"=>$this->dirEnvio))
+        ->add('emailPersona',TextType::Class, array ("data"=>$this->corEnvio))
+        ->add('usuarioPersona',TextType::Class, array ("label"=>"Usuario"))
+        ->add('passPersona',TextType::Class, array ("data"=>"$this->passEnvio"))
+        ->add('estadoPersona')
+        ->add('estadoPersona',ChoiceType::class,array(
+                "label" => "Estado",
+                    "choices"=> array(
+                        "ACTIVO" =>1,
+                        "INACTIVO" =>0,
+              ),
+                'expanded'  => true,
+                'multiple'  => false,
+            ))
+
+            ->add('role', HiddenType::class, array(
+    'data' => 'ROLE_ESTUDIANTE',))
+
+            //->add('idBufete')
+
+
+            ->add('estudiantes', 'BufeteBundle\Form\EstudiantesType', array(
+                'label'=>' ',
+                'carneEnvio' =>$this->carneEnvio,
+            ))
+          ;
     }
-    
+
+
+
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'BufeteBundle\Entity\Personas'
+
+            'data_class' => 'BufeteBundle\Entity\Personas',
+            'carneEnvio'=> null,
+            'nombreEnvio'=> null,
+            'telefonoEnvio'=> null,
+            'direccionEnvio'=> null,
+            'correoEnvio'=> null,
+            'passEnvio' => null,
+
         ));
     }
 
